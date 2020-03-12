71cc9da939dd530033077650b2925e06043a3ff4
Igor Shvetsov <igor.shvetsov@egamings.com>
2020-03-12 11:21:07 +0000

    SCR #93666 - build i18n locales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wlcengine';
+    title = 'wlcengine';
+
+    languageList = [
+        { code: 'en', label: 'English' },
+        { code: 'hi', label: 'हिंदी' },
+        { code: 'es', label: 'Espanol' }
+    ];
+
+    constructor(@Inject(LOCALE_ID) protected localeId: string) {
+
+    }
 }
