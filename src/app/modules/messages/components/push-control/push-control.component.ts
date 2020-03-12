8fe9134bd1b585dd44ef9df5828cd65605828f0f
Igor Shvetsov <igor.shvetsov@egamings.com>
2020-03-12 15:53:52 +0300

    SCR #93666 - modules

---
+++ src/app/modules/messages/components/push-control/push-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-push-control',
+  templateUrl: './push-control.component.html',
+  styleUrls: ['./push-control.component.scss']
+})
+export class PushControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
