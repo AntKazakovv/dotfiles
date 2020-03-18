ea68e1900556f6fcdea093b474b741bea7de35da
Igor Shvetsov <igor.shvetsov@egamings.com>
2020-03-18 16:41:48 +0300

    SCR #93666 - fix routing

--- src/app/modules/pages/layouts/app-layout/app-layout.component.ts
+++ src/app/modules/pages/layouts/app-layout/app-layout.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, LOCALE_ID, Inject} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {StateService} from '@uirouter/core';
 
 @Component({
   selector: 'wlc-app-layout',
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.scss']
 })
-export class AppLayoutComponent implements OnInit {
+export class AppLayoutComponent {
 
-  constructor() { }
+    constructor(
+        @Inject(LOCALE_ID) protected localeId: string,
+        private translate: TranslateService,
+        private stateService: StateService
+    ) {
+        this.setLanguage();
+    }
 
-  ngOnInit(): void {
-  }
+    private setLanguage(): void {
+        this.translate.setDefaultLang('en');
 
+        const params = this.stateService.params;
+        if (params.locale) {
+            this.translate.use(params.locale);
+        }
+    }
 }
