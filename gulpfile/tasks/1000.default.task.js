37aaa3933041340ca799a0726d2347d0496bba23
Maksim Molchanov <maksim.molchanov@egamings.com>
2020-06-30 12:04:23 +0000

    SCR #0 - Fix gulp

--- gulpfile/tasks/1000.default.task.js
+++ gulpfile/tasks/1000.default.task.js
@@ -4,7 +4,7 @@ module.exports = function defaultTask() {
     if(this.params.isEngineBundle) {
         task('default', series('engineBuild'));
     } else {
-        task('default', series('build'));
+        task('default', series('dist'));
     }
 
 }
