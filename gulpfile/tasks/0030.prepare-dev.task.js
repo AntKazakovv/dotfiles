ed0805d9e0b3cd1ce8f272860d7b92f20691ac9c
Maksim Molchanov <maksim.molchanov@egamings.com>
2020-07-06 10:37:43 +0000

    SCR #0 - Chanhe dev task && eng prefix

--- gulpfile/tasks/0030.prepare-dev.task.js
+++ gulpfile/tasks/0030.prepare-dev.task.js
@@ -5,15 +5,15 @@ module.exports = function buildTask() {
     task('prepare:dev', (cb) => {
         this.dev = true;
 
-        // const realEnginePath = `${this.params.paths.engineDev}`;
-        // if (fs.existsSync(realEnginePath)) {
-        //     try {
-        //         fs.unlinkSync(this.params.paths.engine);
-        //     } catch (e) {
-        //         this.deleteFolderRecursive(this.params.paths.engine);
-        //     }
-        //     fs.symlinkSync('../../../wlc-engine', this.params.paths.engine);
-        // }
+        const realEnginePath = `${this.params.paths.engineDev}`;
+        if (fs.existsSync(realEnginePath)) {
+            try {
+                fs.unlinkSync(this.params.paths.engineLink);
+            } catch (e) {
+                this.deleteFolderRecursive(this.params.paths.engineLink);
+            }
+            fs.symlinkSync('../wlc-engine/src', this.params.paths.engineLink);
+        }
 
         cb();
     });
