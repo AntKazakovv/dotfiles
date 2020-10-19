098430206534645128e101d3929498e9ebb73d01
Maksim Molchanov <maksim.molchanov@egamings.com>
2020-10-19 09:10:27 +0000

    SCR #133489 - add faq comp

---
+++ src/modules/static/components/faq/faq.component.ts
@@ -0,0 +1,101 @@
+import {
+    Component,
+    Inject,
+    OnInit,
+    Input,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+} from '@angular/core';
+
+import {AbstractComponent} from 'wlc-engine/classes/abstract.component';
+import {StaticService} from 'wlc-engine/modules/static';
+import {
+    IFaqComponentParams,
+    IFaqData,
+} from './faq.interface';
+import {TextDataModel} from 'wlc-engine/modules/static';
+
+import {
+    map as _map,
+    get as _get,
+    forEach as _forEach,
+    isUndefined as _isUndefined,
+} from 'lodash';
+
+@Component({
+    selector: '[wlc-faq]',
+    templateUrl: './faq.component.html',
+    styleUrls: ['./faq.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FaqComponent extends AbstractComponent implements OnInit {
+    public faqData: IFaqData[];
+
+    @Input() protected slug: string;
+    protected defaultSlug: string = 'partners-faq';
+    protected showErrors: boolean;
+
+    constructor(
+        @Inject('params') protected params: IFaqComponentParams,
+        protected staticService: StaticService,
+        protected cdr: ChangeDetectorRef,
+    ) {
+        super(params);
+    }
+
+    async ngOnInit(): Promise<void> {
+        this.showErrors = _isUndefined(this.params.showErrors) ? true : this.params.showErrors;
+
+        try {
+            const data: TextDataModel = await this.getRawData();
+            this.faqData = this.parseTableData(data?.html);
+            this.cdr.detectChanges();
+        } catch (e) {
+            if (this.showErrors) {
+                console.error('Error post loading');
+            }
+        }
+    }
+
+    public expandToggle(item: IFaqData): void {
+        if(this.params.collapseAll) {
+            const prevState = item.expand;
+            this.foldAll();
+            item.expand = !prevState;
+        } else {
+            item.expand = !item.expand;
+        }
+    }
+
+    protected async getRawData(): Promise<TextDataModel> {
+        return await this.staticService.getPost(this.slug || this.params.slug || this.defaultSlug);
+    }
+
+    protected parseTableData(htmlRow: string): IFaqData[] {
+        const parser: DOMParser = new DOMParser();
+        const doc: Document = parser.parseFromString(htmlRow, 'text/html');
+        const items: NodeList = doc.querySelectorAll('tr');
+
+        if (!items.length) {
+            if (this.showErrors) {
+                console.error('No valid or empty post');
+            }
+            return;
+        }
+
+        return _map(items, (tr: Element) => {
+            const x = tr.querySelectorAll('td');
+            return {
+                title: _get(x, '[0].innerHTML'),
+                content: _get(x, '[1].innerHTML'),
+                expand: false,
+            };
+        });
+    }
+
+    protected foldAll(): void {
+        _forEach(this.faqData, (item: IFaqData) => {
+            item.expand = false;
+        });
+    }
+}
--- src/modules/static/static.module.ts
+++ src/modules/static/static.module.ts
@@ -4,18 +4,21 @@ import {TranslateModule} from '@ngx-translate/core';
 import {UIRouterModule} from '@uirouter/angular';
 import {CoreModule} from 'wlc-engine/modules/core/core.module';
 import {PostComponent} from './components/post/post.component';
+import {FaqComponent} from './components/faq/faq.component';
 
 import {demoTestComponent} from './components/demo-test/demo-test.component';
 
 export const components = {
     'wlc-post': PostComponent,
     'wlc-demo-test': demoTestComponent,
+    'wlc-faq': FaqComponent,
 };
 
 @NgModule({
     declarations: [
         PostComponent,
         demoTestComponent,
+        FaqComponent,
     ],
     id: 'StaticModule',
     imports: [
@@ -27,6 +30,7 @@ export const components = {
     exports: [
         PostComponent,
         demoTestComponent,
+        FaqComponent,
     ]
 })
 export class StaticModule {
