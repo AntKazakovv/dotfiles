52b4a902eafa4e57ff5c6716ca7eb94260551f89
Nikolay Sahankov <nikolay.sahankov@egamings.com>
2021-01-20 14:05:07 +0000

    SCR #146722 - refactor user - info

--- src/modules/app/components/app/app.component.ts
+++ src/modules/app/components/app/app.component.ts
@@ -7,13 +7,15 @@ import {
 } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {StateService, TransitionService, UIRouter, UIRouterGlobals} from '@uirouter/core';
-import {Title} from '@angular/platform-browser';
+import {Title, Meta} from '@angular/platform-browser';
 import {takeUntil} from 'rxjs/operators';
 import {AbstractComponent} from 'wlc-engine/modules/core/system/classes/abstract.component';
 import {SectionModel} from 'wlc-engine/modules/core/system/models/section.model';
 import {ConfigService, LayoutService} from '../../../core/system/services';
 import {ILanguage} from 'wlc-engine/modules/core';
 import {DeviceModel} from 'wlc-engine/modules/core';
+import {fromEvent} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 import {
     sortBy as _sortBy,
@@ -47,6 +49,7 @@ export class AppComponent extends AbstractComponent implements OnInit, OnDestroy
         protected cdr: ChangeDetectorRef,
         private transition: TransitionService,
         private titleService: Title,
+        private meta: Meta,
     ) {
         super({injectParams: {}, defaultParams}, configService);
         const currentLang = router.stateService.params?.locale || 'en';
@@ -86,7 +89,12 @@ export class AppComponent extends AbstractComponent implements OnInit, OnDestroy
             this.sections.push(...sections);
         });
         this.setHostClass();
+        this.updateMetaTag();
         this.cdr.markForCheck();
+
+        fromEvent(window, 'resize').pipe(debounceTime(500)).subscribe(() => {
+            this.updateMetaTag();
+        });
     }
 
     public trackBySectionName(index: number, section: SectionModel): string {
@@ -101,5 +109,19 @@ export class AppComponent extends AbstractComponent implements OnInit, OnDestroy
             `${_get(this.uiRouter, '$current.name', '').replace(/\./g, '-')}-state`,
         ];
         this.$hostClass = hostClass.join(' ');
+    }
+
+    private updateMetaTag(): void {
+        if (window.innerWidth > 375) {
+            this.meta.updateTag({
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            });
+        } else {
+            this.meta.updateTag({
+                name: 'viewport',
+                content: 'width=375',
+            });
+        }
     }
 }
