bd8a89224e40cc25841d6c99c4cf44781feb59e7
Nikolay Sakhankov <nikolay.sahankov@egamings.com>
2021-02-01 11:31:18 +0300

    SCR #0 - update profile module

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,38 +1,23 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {
-    BannersService,
-    WinnersService,
-} from './system/services';
-import {SwiperModule} from 'ngx-swiper-wrapper';
-import {BannerComponent} from 'wlc-engine/modules/promo/components/banner/banner.component';
-import {SliderComponent} from 'wlc-engine/modules/promo/components/slider/slider.component';
-import {UserModule} from 'wlc-engine/modules/user/user.module';
-import {CoreModule} from 'wlc-engine/modules/core/core.module';
-
+import {TranslateModule} from "@ngx-translate/core";
+import {UIRouterModule} from "@uirouter/angular";
+import {CoreModule} from "wlc-engine/modules/core/core.module";
 export const components = {
-    'wlc-slider': SliderComponent,
-    'wlc-banner': BannerComponent,
 };
 
 @NgModule({
     declarations: [
-        SliderComponent,
-        BannerComponent,
     ],
     imports: [
         CommonModule,
-        SwiperModule,
-        UserModule,
+        TranslateModule,
+        UIRouterModule,
         CoreModule,
     ],
     providers: [
-        BannersService,
-        WinnersService,
     ],
     exports: [
-        SliderComponent,
-        BannerComponent,
     ],
 })
 export class ProfileModule {}
