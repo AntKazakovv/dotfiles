54848b9d8fb4bcee87d5668223aafbd3ee911572
Mikhail Kuznetsov <mikhail.kuznetsov@egamings.com>
2021-02-04 14:39:22 +0300

    SCR #186924 - config export

--- src/modules/core/system/services/config/config.interface.ts
+++ src/modules/core/system/services/config/config.interface.ts
@@ -4,6 +4,8 @@ import {
     ILoyaltyConfig,
 } from 'wlc-engine/modules/core/system/interfaces';
 import {AppConfigModel} from 'wlc-engine/modules/core';
+import {IGamesConfig} from 'wlc-engine/modules/games/system/interfaces/games.interfaces';
+import {IStaticConfig} from 'wlc-engine/modules/static/system/interfaces/static.interface';
 
 /**
  * Types of storage to get or set data
@@ -11,12 +13,13 @@ import {AppConfigModel} from 'wlc-engine/modules/core';
 export type IStorageType = 'localStorage' | 'sessionStorage' | 'cookiesStorage';
 
 export interface IGlobalConfig {
-    appConfig: AppConfigModel,
-    $layouts: ILayoutsConfig,
-    $static: unknown,
-    $files: IIndexing<string>,
-    $games: unknown,
-    $loyalty: ILoyaltyConfig,
+    appConfig: AppConfigModel;
+    $layouts: ILayoutsConfig;
+    $panelsLayouts: ILayoutsConfig;
+    $static: IStaticConfig;
+    $files: IIndexing<string>;
+    $games: IGamesConfig;
+    $loyalty: ILoyaltyConfig;
 }
 
 export interface IGetParams {
