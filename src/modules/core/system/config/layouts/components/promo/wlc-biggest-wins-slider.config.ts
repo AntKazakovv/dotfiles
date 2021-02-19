854ebb2f86456e69e81fad50c71d5e56316ab725
Maksim Molchanov <maksim.molchanov@egamings.com>
2021-02-19 06:13:58 +0000

    SCR #191434 - Some fix

--- src/modules/core/system/config/layouts/components/promo/wlc-biggest-wins-slider.config.ts
+++ src/modules/core/system/config/layouts/components/promo/wlc-biggest-wins-slider.config.ts
@@ -7,7 +7,7 @@ export namespace wlcBiggestWinsSlider {
             type: 'biggest',
             theme: 'vertical',
             title: 'Biggest wins',
-            wlcElement: 'section_biggest-wins'
+            wlcElement: 'section_biggest-wins',
         },
     };
 }
