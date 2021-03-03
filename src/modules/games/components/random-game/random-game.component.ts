b23f0fe6bc33fe0eaa08ed7ca5b0155c5dfa95ee
Ivan Kuzmichev <ivan.kuzmichev@egamings.com>
2021-03-03 15:35:02 +0000

    SCR #0 - edits random game

--- src/modules/games/components/random-game/random-game.component.ts
+++ src/modules/games/components/random-game/random-game.component.ts
@@ -1,16 +1,25 @@
 import {
     ChangeDetectionStrategy,
-    ChangeDetectorRef,
     Component,
     Inject,
     Input,
     OnInit,
 } from '@angular/core';
-import {ConfigService, GlobalHelper} from 'wlc-engine/modules/core';
+import {
+    ConfigService,
+    EventService,
+    GlobalHelper,
+    ModalService,
+} from 'wlc-engine/modules/core';
 import {AbstractComponent} from 'wlc-engine/modules/core/system/classes/abstract.component';
 import {GamesCatalogService} from 'wlc-engine/modules/games';
 import {Game} from 'wlc-engine/modules/games/system/models/game.model';
+import {IPushMessageParams, NotificationEvents} from 'wlc-engine/modules/core/system/services/notification';
 import * as Params from './random-game.params';
+
+import {
+    filter as _filter,
+} from 'lodash-es';
 
 @Component({
     selector: '[wlc-random-game]',
@@ -22,6 +31,7 @@ export class RandomGameComponent extends AbstractComponent implements OnInit {
 
     public $params: Params.IRandomGameCParams;
     protected games: Game[];
+    protected gamesForAuthorized: boolean = false;
 
     @Input() protected inlineParams: Params.IRandomGameCParams;
 
@@ -29,7 +39,8 @@ export class RandomGameComponent extends AbstractComponent implements OnInit {
         @Inject('injectParams') protected injectParams: Params.IRandomGameCParams,
         protected gamesCatalogService: GamesCatalogService,
         protected configService: ConfigService,
-        protected cdr: ChangeDetectorRef,
+        protected modalService: ModalService,
+        protected eventService: EventService,
     ) {
         super({
             injectParams,
@@ -42,14 +53,34 @@ export class RandomGameComponent extends AbstractComponent implements OnInit {
     }
 
     public toRandomGame(): void {
+        const auth = this.configService.get<boolean>('$user.isAuthenticated');
 
-        if (!this.games) {
-            this.games = this.gamesCatalogService.getGameList();
+        if (!this.games || this.gamesForAuthorized !== auth) {
+            this.getGames(auth);
         }
 
-        const gameNumber: number = GlobalHelper.randomNumber(0, this.games.length);
-        this.games[gameNumber].launch({
-            demo: !this.configService.get<boolean>('$user.isAuthenticated'),
-        });
+        if (this.games.length) {
+            const gameNumber: number = GlobalHelper.randomNumber(0, this.games.length - 1);
+            this.games[gameNumber].launch({
+                demo: !auth,
+            });
+        } else if (!auth) {
+            this.modalService.showModal('login');
+        } else {
+            this.eventService.emit({
+                name: NotificationEvents.PushMessage,
+                data: <IPushMessageParams> {
+                    type: 'error',
+                    title: gettext('Failed to open game'),
+                    message: gettext('Sorry, something went wrong!'),
+                },
+            });
+        }
+    }
+
+    protected getGames(auth: boolean): void {
+        const games = this.gamesCatalogService.getGameList();
+        this.games = auth ? games : _filter(games, {hasDemo: true});
+        this.gamesForAuthorized = auth;
     }
 }
