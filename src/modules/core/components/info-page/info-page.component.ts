b173b65163886368cccbfe0cbefed769571b1c96
Nadezhda Babicheva <nadezhda.babicheva@egamings.com>
2021-04-30 15:01:55 +0000

    SCR #212556 - contacts can be removed from wlc-info via config

--- src/modules/core/components/info-page/info-page.component.ts
+++ src/modules/core/components/info-page/info-page.component.ts
@@ -12,6 +12,7 @@ import {
 import {
     IWrapperCParams,
     AbstractComponent,
+    ConfigService,
 } from 'wlc-engine/modules/core';
 import * as Params from './info-page.params';
 
@@ -44,8 +45,9 @@ export class InfoPageComponent extends AbstractComponent implements OnInit {
         private uiRouter: UIRouterGlobals,
         private transition: TransitionService,
         protected cdr: ChangeDetectorRef,
+        protected configService: ConfigService,
     ) {
-        super({injectParams: params, defaultParams: Params.defaultParams});
+        super({injectParams: params, defaultParams: Params.defaultParams}, configService);
     }
 
     public ngOnInit(): void {
