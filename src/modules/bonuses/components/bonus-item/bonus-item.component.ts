487183324ab4c77b076a7de6517f73fd19d720ce
Tatyana Kotvickaya <tatyana.kotvickaya@egamings.com>
2021-05-18 14:31:03 +0300

    SCR #224097 - [fix] bug bonuses actions

--- src/modules/bonuses/components/bonus-item/bonus-item.component.ts
+++ src/modules/bonuses/components/bonus-item/bonus-item.component.ts
@@ -184,15 +184,17 @@ export class BonusItemComponent extends AbstractComponent implements OnInit, OnD
     }
 
     public async getInventory(): Promise<void> {
-        this.bonus = await this.bonusesService.takeInventory(this.bonus);
-        if (this.bonus) {
+        const bonus = await this.bonusesService.takeInventory(this.bonus);
+        if (bonus) {
+            this.bonus = bonus;
             this.cdr.markForCheck();
         }
     }
 
     public async join(): Promise<void> {
-        this.bonus = await this.bonusesService.subscribeBonus(this.bonus);
-        if (this.bonus) {
+        const bonus = await this.bonusesService.subscribeBonus(this.bonus);
+        if (bonus) {
+            this.bonus = bonus;
             this.bonusesService.clearPromoBonus();
             this.cdr.markForCheck();
         }
@@ -214,8 +216,9 @@ export class BonusItemComponent extends AbstractComponent implements OnInit, OnD
             confirmBtnText: gettext('Yes'),
             textAlign: 'center',
             onConfirm: async () => {
-                this.bonus = await this.bonusesService.cancelBonus(this.bonus);
-                if (this.bonus) {
+                const bonus = await this.bonusesService.cancelBonus(this.bonus);
+                if (bonus) {
+                    this.bonus = bonus;
                     this.cdr.markForCheck();
                 }
             },
@@ -224,9 +227,10 @@ export class BonusItemComponent extends AbstractComponent implements OnInit, OnD
     }
 
     public async unsubscribe(): Promise<void> {
-        this.bonus = await this.bonusesService.unsubscribeBonus(this.bonus);
+        const bonus = await this.bonusesService.unsubscribeBonus(this.bonus);
 
-        if (this.bonus) {
+        if (bonus) {
+            this.bonus = bonus;
             this.cdr.markForCheck();
         }
     }
