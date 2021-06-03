51cc9ff00bae7cad7a9f7dcdbd7f90c204195267
Klara Aslanova <klara.aslanova@egamings.com>
2021-06-03 12:52:24 +0300

    SCR #216853 - serach button version fix

--- src/modules/core/system/config/layouts/sections/categories.section.ts
+++ src/modules/core/system/config/layouts/sections/categories.section.ts
@@ -34,7 +34,7 @@ export namespace categories {
                             params: {
                                 class: 'games-categories__wrapper',
                                 components: [
-                                    componentLib.wlcButton.searchV3,
+                                    componentLib.wlcButton.search,
                                     componentLib.wlcButton.searchMerchants,
                                 ],
                             },
