546759a348b4aa45056585fff7f420a3eff983df
Maksim Molchanov <maksim.molchanov@egamings.com>
2021-07-26 15:23:56 +0000

    SCR #248271 - [fix] Move log-types to the submodule path

--- src/modules/core/system/config/log-types/6.page-not-found.ts
+++
@@ -1,8 +0,0 @@
-import {ILogTypes} from 'wlc-engine/modules/core/system/config/log-types';
-
-export const errorTypes: ILogTypes = {
-    '6.0.0': {
-        // Page not found
-        level: 'error',
-    },
-};
--- src/modules/core/system/config/log-types/9.verification.ts
+++
@@ -1,8 +0,0 @@
-import {ILogTypes} from 'wlc-engine/modules/core/system/config/log-types';
-
-export const errorTypes: ILogTypes = {
-    '9.0.1': {
-        // No document type name
-        level: 'warning',
-    },
-};
