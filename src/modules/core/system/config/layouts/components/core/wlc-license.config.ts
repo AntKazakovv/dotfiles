b0bc2db8664356cfabc13b18059d8b19e308d2ac
Dmitry Konovalov <dmitry.konovalov@egamings.com>
2021-08-16 09:52:09 +0300

    SCR #254419 - [FIX] default footer license adaptive

--- src/modules/core/system/config/layouts/components/core/wlc-license.config.ts
+++ src/modules/core/system/config/layouts/components/core/wlc-license.config.ts
@@ -8,14 +8,14 @@ export namespace wlcLicense {
     export const onlyDesktop: ILayoutComponent = {
         name: 'core.wlc-license',
         display: {
-            after: 1024,
+            after: 900,
         },
     };
 
     export const onlyMobile: ILayoutComponent = {
         name: 'core.wlc-license',
         display: {
-            before: 1023,
+            before: 899,
         },
     };
 }
