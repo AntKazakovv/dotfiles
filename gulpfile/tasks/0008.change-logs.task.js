8812140d4437ddc58318957cd65218dd1b026448
Mikhail Kuznetsov <mikhail.kuznetsov@egamings.com>
2021-09-02 17:56:25 +0300

    SCR #262136 - fix: change log generate after transfer repository to wlcgitlab

--- gulpfile/tasks/0008.change-logs.task.js
+++ gulpfile/tasks/0008.change-logs.task.js
@@ -19,12 +19,28 @@ module.exports = function changeLogsTask() {
             throw Error('Parameter tag (id of release tag) is empty');
         }
 
+        //get repository url
+        const origin = this.execNativeShellSync('git remote get-url origin').trim();
+        const originMath = origin.match(/\@(.*)\:/);
+        const gitUrl = _.isArray(originMath) ? originMath[1] : 'wlcgitlab.egamings.com';
+
+
         let apiKey = '';
         if (fs.existsSync('.gitlab-key')) {
             apiKey = _.trim(fs.readFileSync('.gitlab-key'));
         } else {
-            throw Error('Gitlab api key not found. See https://vcsxa.egamings.com/wlc/devtools/-/blob/master/theme/change-logs.md');
+            throw Error(
+                `Gitlab api key not found. See https://${gitUrl}/wlc/devtools/-/blob/master/theme/change-logs.md`,
+            );
         }
+
+        const searchUrl = `curl "https://wlcgitlab.egamings.com/api/v4/projects/?private_token=${apiKey}`
+            + '&search='
+            + encodeURIComponent('wlc-engine')
+            + '"';
+
+        const response = this.execNativeShellSync(searchUrl);
+        const projectId = _.find(JSON.parse(response), (item) => item.ssh_url_to_repo === origin).id;
 
         const commitHistory = this.execNativeShellSync('git log --oneline --format="%s" -100');
         const history = commitHistory.split('\n');
@@ -45,7 +61,9 @@ module.exports = function changeLogsTask() {
 
             if (_.includes(commit.title, 'SCR #')) {
                 const searchText = encodeURIComponent(commit.title);
-                const response = this.execNativeShellSync(`curl "https://vcsxa.egamings.com/api/v4/projects/315/merge_requests?state=merged&search=${searchText}&private_token=${apiKey}"`);
+                const response = this.execNativeShellSync(`curl "https://${gitUrl}/api/v4/projects/`
+                    + `${projectId}/merge_requests?state=merged&search=${searchText}&private_token=${apiKey}"`);
+
                 const data = JSON.parse(response);
                 if (_.isArray(data)) {
                     commit.description = _.get(data, '[0].description', '');
@@ -56,7 +74,9 @@ module.exports = function changeLogsTask() {
                     break;
                 }
                 const sourceBranch = sourceBranchArr[0];
-                const response = this.execNativeShellSync(`curl "https://vcsxa.egamings.com/api/v4/projects/315/merge_requests?state=merged&source_branch=${sourceBranch}&private_token=${apiKey}"`);
+                const response = this.execNativeShellSync(`curl "https://${gitUrl}/api/v4/projects`
+                    + `/${projectId}/merge_requests?state=merged`
+                    + `&source_branch=${sourceBranch}&private_token=${apiKey}"`);
                 const data = JSON.parse(response);
                 if (_.isArray(data)) {
                     commit.title = _.get(data, '[0].title', '');
@@ -80,6 +100,7 @@ module.exports = function changeLogsTask() {
                 const commitTitle = commit.title.replace(/#(\d+)/, '[#$1](https://tracker.egamings.com/issues/$1)');
                 content += `### ${commitTitle}${commit.description ? '\n' + _.trim(commit.description) : ''}\n\n`;
             }
+            // eslint-disable-next-line no-console
             console.log(`\nChanges of release\n\n${content}`);
 
             let response;
@@ -87,13 +108,13 @@ module.exports = function changeLogsTask() {
                 response = await inquirer.prompt({
                     type: 'confirm',
                     name: 'create',
-                    message: `Changes will be saved in ./${relativeFilePath}, continue?`
+                    message: `Changes will be saved in ./${relativeFilePath}, continue?`,
                 });
             } else {
                 response = await inquirer.prompt({
                     type: 'confirm',
                     name: 'create',
-                    message: `No changes, continue?`
+                    message: 'No changes, continue?',
                 });
             }
             if (!response.create) {
@@ -101,6 +122,7 @@ module.exports = function changeLogsTask() {
             }
 
             fs.writeFileSync(filePath, content);
+            // eslint-disable-next-line no-console
             console.log(`Changes history saved to ${filePath}`);
         } else {
             throw Error(`Directory ./${relativeDirPath} not founded`);
@@ -110,34 +132,34 @@ module.exports = function changeLogsTask() {
 
         src([
             `${this.params.paths.changeLogs}/*.md`,
-            `!${this.params.paths.changeLogs}/*index.md`
+            `!${this.params.paths.changeLogs}/*index.md`,
         ])
-        .pipe(sort({
-            asc: false,
-        }))
-        .pipe(customFilter(file => {
-            const fileNameInfo = path.basename(file.basename, '.md').split('_');
-            const dateInfo = fileNameInfo[1].split('-');
-            const fileDate = luxon.DateTime.fromISO(`${dateInfo[2]}-${dateInfo[1]}-${dateInfo[0]}`);
-            const interval = luxon.Interval.fromDateTimes(fileDate, fileDate);
+            .pipe(sort({
+                asc: false,
+            }))
+            .pipe(customFilter(file => {
+                const fileNameInfo = path.basename(file.basename, '.md').split('_');
+                const dateInfo = fileNameInfo[1].split('-');
+                const fileDate = luxon.DateTime.fromISO(`${dateInfo[2]}-${dateInfo[1]}-${dateInfo[0]}`);
+                const interval = luxon.Interval.fromDateTimes(fileDate, fileDate);
 
-            if (interval.isAfter(minStartDate)) {
-                return true;
-            }
-            return false;
-        }))
-        .pipe(modifyFile((content, filePath, file) => {
-            const fileNameInfo = path.basename(filePath, '.md').split('_');
-            const tag = fileNameInfo[0];
-            const date = fileNameInfo[1];
-            return `## ${tag} (${date})\n\n${content}`;
-        }))
-        .pipe(concat('900.change-logs.md'))
+                if (interval.isAfter(minStartDate)) {
+                    return true;
+                }
+                return false;
+            }))
+            .pipe(modifyFile((content, filePath, file) => {
+                const fileNameInfo = path.basename(filePath, '.md').split('_');
+                const tag = fileNameInfo[0];
+                const date = fileNameInfo[1];
+                return `## ${tag} (${date})\n\n${content}`;
+            }))
+            .pipe(concat('900.change-logs.md'))
             .pipe(dest(this.params.paths.changeLogsDocDist))
             .pipe(markdown())
             .pipe(modifyFile((content, filePath, file) => {
                 return `\ufeff<html><head></head><body>${content}</body></html>`;
-        }));
+            }));
 
         done();
     });
