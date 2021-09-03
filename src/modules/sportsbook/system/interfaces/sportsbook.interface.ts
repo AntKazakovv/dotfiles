471c21bfc40a8f38b4fc55c7efa01a5aef5b44f2
Valery Nochvin <valery.nochvin@softgamings.com>
2021-09-03 15:55:10 +0300

    SCR #244168 - feat: expanded the possibilities of sportbool config

--- src/modules/sportsbook/system/interfaces/sportsbook.interface.ts
+++ src/modules/sportsbook/system/interfaces/sportsbook.interface.ts
@@ -16,8 +16,18 @@ export interface ISportsbookSettingsFilter {
 }
 
 export interface IBetradar {
+    /**
+     * `cssfile: static/config/fileName.css` - gets the path to the style file in sportsbook
+     */
     cssFile?: string;
+    /**
+     * `configFile: static/config/fileName.js` - gets the path to the js file with sportsbook configurations
+     */
     configFile?: string;
+    /**
+     * ``theme: v2` - the theme of the sportsbook that will be used by default. If there is no field, the first topic will be selected.To use the second theme, you need to register the value "v2".
+     */
+    theme?: string;
     widgets?: {
         env: IIndexing<IBetradarWidgetEnvironment>;
         dailyMatch?: IBetradarDailyMatch;
