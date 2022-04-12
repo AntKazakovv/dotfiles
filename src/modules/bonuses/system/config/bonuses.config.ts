c3c44ea6f62c24f2cd094ee61c8a178050c34e0a
dgavrilov <dmitry.gavrilov@softgamings.com>
2022-04-12 07:25:49 +0000

    SCR #367708 - fix: remove default bonus images

--- src/modules/bonuses/system/config/bonuses.config.ts
+++ src/modules/bonuses/system/config/bonuses.config.ts
@@ -2,17 +2,6 @@ import {IBonusesModule} from 'wlc-engine/modules/bonuses/system/interfaces/bonus
 
 export const bonusesConfig: IBonusesModule = {
     useIconBonusImage: true,
-    defaultImages: {
-        image: '/gstatic/wlc/bonuses/bonus-bg-haze-promo.png',
-        imageProfileFirst: '/gstatic/wlc/bonuses/bonus-bg-horizontal.png',
-        imageReg: '/gstatic/wlc/bonuses/bonus-bg-haze-vertical.png',
-        imagePromo: '/gstatic/wlc/bonuses/bonus-bg-promo-second.png',
-        imageOther: '/gstatic/wlc/bonuses/modal-bonus-default.png',
-        imageStore: '',
-        imagePromoHome: '/gstatic/wlc/bonuses/no-bonuses/no-bonus-block-background.jpg',
-        imageBlank: '/gstatic/wlc/bonuses/blank-bonus-decor.png',
-        imageDummy: '/gstatic/wlc/bonuses/bonus-dummy.svg',
-    },
     components: {
         'wlc-bonuses-list': {
             noContent: {
