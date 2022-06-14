e3e2fb7b2188571cec3b2aaea2f8b0e0bc057a71
Dmitry Bunakov <dmitry.bunakov@softgamings.com>
2022-06-14 09:58:51 +0300

    SCR #85647 - fix: show message on bootrstap request fail

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,89 +1,114 @@
-declare function require(path: string): {
-    [key: string]: any;
-    default: string;
-};
-declare function gettext<T>(str: T): T;
+import {PreloaderLogo} from 'wlc-engine/system/inline/_preloader-logo';
 
-declare interface IWlcPostMessage {
-    event: string;
-    eventData?: string;
-}
+export declare global {
 
-declare interface ISentryConfig {
-    project?: string;
-}
+    function require(path: string): {
+        [key: string]: any;
+        default: string;
+    };
 
-declare interface IVerboxSetup {
-    domain?: string;
-    language?: string;
-    clientId?: number;
-    returnMobileTriggerTimeout?: number;
-}
+    function gettext<T>(str: T): T;
 
-declare interface IScreenfull {
-    isFullscreen: boolean;
-    element: Element | undefined;
-    isEnabled: boolean;
-    raw: ScreenfullRawEventNames;
-    request(element?: Element, options?: FullscreenOptions): Promise<void>;
-    exit(): Promise<void>;
-    toggle(element?: Element, options?: FullscreenOptions): Promise<void>;
-    on(name: ScreenfullEventName, handler: (event: Event) => void): void;
-    off(name: ScreenfullEventName, handler: (event: Event) => void): void;
-    onchange(handler: (event: Event) => void): void;
-    onerror(handler: (event: Event) => void): void;
-}
+    interface IScreenfull {
+        isFullscreen: boolean;
+        element: Element | undefined;
+        isEnabled: boolean;
+        raw: ScreenfullRawEventNames;
+        request(element?: Element, options?: FullscreenOptions): Promise<void>;
+        exit(): Promise<void>;
+        toggle(element?: Element, options?: FullscreenOptions): Promise<void>;
+        on(name: ScreenfullEventName, handler: (event: Event) => void): void;
+        off(name: ScreenfullEventName, handler: (event: Event) => void): void;
+        onchange(handler: (event: Event) => void): void;
+        onerror(handler: (event: Event) => void): void;
+    }
+    interface IWlcPostMessage {
+        event: string;
+        eventData?: string;
+    }
 
-declare type ScreenfullEventName = 'change' | 'error';
+    interface ISentryConfig {
+        project?: string;
+    }
 
-declare type ScreenfullRawEventNames = {
-    readonly requestFullscreen: string;
-    readonly exitFullscreen: string;
-    readonly fullscreenElement: string;
-    readonly fullscreenEnabled: string;
-    readonly fullscreenchange: string;
-    readonly fullscreenerror: string;
-};
+    interface IVerboxSetup {
+        domain?: string;
+        language?: string;
+        clientId?: number;
+        returnMobileTriggerTimeout?: number;
+    }
 
-declare type TExternalSBPageSwitch = (pageId: number) => void;
+    interface IPaymentIQCashier {
+        new (el: string, cashierConfig: IPiqCashierConfig, callback?: Function);
+    }
 
-declare type TMethodName = 'fbq' | 'gtag';
+    interface IScreenfull {
+        isFullscreen: boolean;
+        element: Element | undefined;
+        isEnabled: boolean;
+        raw: ScreenfullRawEventNames;
+        request(element?: Element, options?: FullscreenOptions): Promise<void>;
+        exit(): Promise<void>;
+        toggle(element?: Element, options?: FullscreenOptions): Promise<void>;
+        on(name: ScreenfullEventName, handler: (event: Event) => void): void;
+        off(name: ScreenfullEventName, handler: (event: Event) => void): void;
+        onchange(handler: (event: Event) => void): void;
+        onerror(handler: (event: Event) => void): void;
+    }
 
-declare type TAnalyticMethod = {
-    [key in TMethodName]: Function;
-}
+    type ScreenfullEventName = 'change' | 'error';
 
-declare interface Window extends TAnalyticMethod {
-    WLC_VERSION: number;
-    WLC_ENV?: string;
-    WLC_FORBIDDEN?: boolean;
-    wlcSentryConfig?: ISentryConfig;
-    testSessionHash?: string;
-    wlcPreload: any;
-    Fingerprint2?: any;
-    fingerprintHash?: string;
-    requestIdleCallback?: any;
-    Chatra?: any;
-    ChatraSetup?: any;
-    ChatraID?: string;
-    ChatraGroupID?: string;
-    Verbox?: any;
-    supportAPIMethod?: string;
-    VerboxSetup?: IVerboxSetup;
-    Tawk_API?: any,
-    __lc?: any;
-    LC_API?: any;
-    LiveChatWidget?: any;
-    affiliate: string;
-    WlcFlog: any;
-    WlcCookie: any;
-    WlcHelper: any;
-    affCookie: any;
-    _PaymentIQCashierReset?: () => void;
-    zESettings?: any;
-    zE?: any;
-    zEACLoaded?: boolean;
-    grecaptcha?: ReCaptchaV2.ReCaptcha;
-    // change page of tglab sportsbook
-    externalSBPageSwitch?: TExternalSBPageSwitch;
+    type ScreenfullRawEventNames = {
+        readonly requestFullscreen: string;
+        readonly exitFullscreen: string;
+        readonly fullscreenElement: string;
+        readonly fullscreenEnabled: string;
+        readonly fullscreenchange: string;
+        readonly fullscreenerror: string;
+    };
+
+    type TExternalSBPageSwitch = (pageId: number) => void;
+
+    type TMethodName = 'fbq' | 'gtag';
+
+    type TAnalyticMethod = {
+        [key in TMethodName]: Function;
+    }
+
+    interface Window extends TAnalyticMethod {
+        WLC_VERSION: number;
+        WlcPreloaderLogo: PreloaderLogo;
+        WLC_ENV?: string;
+        WLC_FORBIDDEN?: boolean;
+        wlcSentryConfig?: ISentryConfig;
+        testSessionHash?: string;
+        wlcPreload: any;
+        Fingerprint2?: any;
+        fingerprintHash?: string;
+        requestIdleCallback?: any;
+        Chatra?: any;
+        ChatraSetup?: any;
+        ChatraID?: string;
+        ChatraGroupID?: string;
+        Verbox?: any;
+        supportAPIMethod?: string;
+        VerboxSetup?: IVerboxSetup;
+        Tawk_API?: any,
+        __lc?: any;
+        LC_API?: any;
+        LiveChatWidget?: any;
+        affiliate: string;
+        WlcFlog: any;
+        WlcCookie: any;
+        WlcHelper: any;
+        affCookie: any;
+        _PaymentIQCashier?: IPaymentIQCashier;
+        _PaymentIQCashierReset?: any;
+        zESettings?: any;
+        zE?: any;
+        zEACLoaded?: boolean;
+        grecaptcha?: ReCaptchaV2.ReCaptcha;
+        // change page of tglab sportsbook
+        externalSBPageSwitch?: TExternalSBPageSwitch;
+    }
 }
