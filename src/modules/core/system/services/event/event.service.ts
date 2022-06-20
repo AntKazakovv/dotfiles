f980dd27565c6f170e68a337ea88d6143721902b
Andrey Kudrenko <andrey.kudrenko@softgamings.com>
2022-06-20 23:01:26 +0300

    SCR #376276 - refactor: event service refactoring

--- src/modules/core/system/services/event/event.service.ts
+++ src/modules/core/system/services/event/event.service.ts
@@ -1,67 +1,90 @@
 import {Injectable} from '@angular/core';
-import {Subject, Observable, pipe, UnaryFunction, Subscription, PartialObserver} from 'rxjs';
-import {filter, map, takeUntil} from 'rxjs/operators';
 
-import _get from 'lodash-es/get';
+import {
+    Observable,
+    PartialObserver,
+    pipe,
+    Subject,
+    Subscription,
+    UnaryFunction,
+} from 'rxjs';
+import {
+    filter,
+    map,
+    takeUntil,
+} from 'rxjs/operators';
+
 import _assign from 'lodash-es/assign';
+import _every from 'lodash-es/every';
+import _get from 'lodash-es/get';
 import _isArray from 'lodash-es/isArray';
+import _keys from 'lodash-es/keys';
+import _omit from 'lodash-es/omit';
+import _some from 'lodash-es/some';
 
 export type EventType = 'event' | 'error' | 'system';
 
-export interface IFilterParams {
+export interface IEventFilter {
     name: string;
     type?: EventType;
     from?: string;
     status?: string;
 }
 
-export type FilterType = Partial<IFilterParams> | Partial<IFilterParams>[];
-
-export interface IEvent<T> extends IFilterParams {
+export interface IEvent<T> extends IEventFilter {
     data?: T;
 }
 
+type TFilterEventsFunction<T> = UnaryFunction<Observable<IEvent<T>>, Observable<IEvent<T>>> ;
+
+type TFiltersArray = Partial<IEventFilter>[];
+
+type TFilterType = Partial<IEventFilter> | TFiltersArray;
+
+type TSubscriber<T> = (data: T) => void | PartialObserver<T>;
+
+type TUnknownEvent = IEvent<unknown>;
+
+type TUntil = Observable<unknown>;
+
 @Injectable()
 export class EventService {
+    /**
+     * Memoizing storage for event pipes
+     */
+    protected cache = new Map<string, TFilterEventsFunction<unknown>>();
+
+    protected flow$: Subject<TUnknownEvent> = new Subject<TUnknownEvent>();
 
     /**
      * Observable flow of all the events
      *
-     * @return {Observable<IEvent<unknown>>}
+     * @return {Observable<TUnknownEvent>}
      */
-    public get flow(): Observable<IEvent<unknown>> {
+    public get flow(): Observable<TUnknownEvent> {
         return this.flow$.asObservable();
-    }
-
-    private flow$: Subject<IEvent<unknown>> = new Subject<IEvent<unknown>>();
-
-    constructor() {
     }
 
     /**
      * Emits the events
      *
-     * @param {IEvent<unknown>} event
+     * @param {TUnknownEvent} event
      */
-    public emit(event: IEvent<unknown>): void {
-        this.flow$.next(
-            _assign({type: 'event'}, event),
-        );
+    public emit(event: TUnknownEvent): void {
+        this.flow$.next(_assign({type: 'event'}, event));
     }
 
     /**
      * Filter the events
      *
-     * @param {FilterType} type of event filter
+     * @param {TFilterType} type of event filter
      * @param {Observable<any>} until
      *
      * @returns {Observable<IEvent<T>>}
      */
-    public filter<T>(params: FilterType, until?: Observable<any>): Observable<IEvent<T>> {
+    public filter<T>(filter: TFilterType, until?: TUntil): Observable<IEvent<T>> {
         return this.flow.pipe(
-            this.filterEvents<T>(
-                _isArray(params) ? params : [params],
-            ),
+            this.filterEvents<T>(_isArray(filter) ? filter : [filter]),
             (until) ? takeUntil(until) : pipe(),
         );
     }
@@ -69,37 +92,61 @@ export class EventService {
     /**
      * Subscribe to the events
      *
-     * @param {FilterType} type of event filter
-     * @param {(data: T) => void | PartialObserver<T>} subscriber
+     * @param {TFilterType} type of event filter
+     * @param {TSubscriber} subscriber
      * @param {Observable<any>} until
      *
      * @returns Subscription
      */
-    public subscribe<T>(
-        eventFilter: FilterType,
-        subscriber: (data: T) => void | PartialObserver<T>,
-        until?: Observable<any>,
-    ): Subscription {
-        return this.filter(eventFilter, until)
-            .pipe(
-                map((event: IEvent<T>): T => _get(event, 'data')),
-            )
+    public subscribe<T>(filter: TFilterType, subscriber: TSubscriber<T>, until?: TUntil): Subscription {
+        return this.filter<T>(filter, until)
+            .pipe(map((event: IEvent<T>): T => _get(event, 'data')))
             .subscribe(subscriber);
     }
 
-    private filterEvents<T>(
-        params: Partial<IFilterParams>[],
-    ): UnaryFunction<Observable<IEvent<T>>, Observable<IEvent<T>>> {
-        return pipe(filter((event: IEvent<T>) => {
-            return params.reduce((result, eventFilter) => {
-                return result || this.filterEvent(eventFilter, event);
-            }, false);
-        }));
+    protected filterEvents<T>(filters: Partial<IEvent<T>>[]): TFilterEventsFunction<T> {
+        const key = EventService.generateFiltersKey(filters);
+
+        try {
+            return this.getMemoized(key);
+        } catch {
+            const filterFunction = pipe(
+                filter((event: IEvent<T>) => (
+                    _some(filters, (filter: IEvent<T>) => EventService.isEventMatching(filter, event))
+                )),
+            );
+
+            this.setMemoized(key, filterFunction);
+
+            return filterFunction;
+        }
     }
 
-    private filterEvent(filterParam: Partial<IFilterParams>, event: IFilterParams): boolean {
-        return Object.keys(filterParam).reduce((result: boolean, key: string) => {
-            return result && event[key] === filterParam[key];
-        }, true);
+    protected getMemoized<T>(key: string): TFilterEventsFunction<T> {
+        if (this.cache.has(key)) {
+            return this.cache.get(key) as TFilterEventsFunction<T>;
+        }
+
+        throw new Error(`Cannot find memoized filter by key "${key}"`);
+    }
+
+    protected setMemoized<T>(key: string, value: TFilterEventsFunction<T>): void {
+        this.cache.set(key, value);
+    }
+
+    private static generateFiltersKey<T>(filters: Partial<IEvent<T>>[]): string {
+        const stringified = filters.map((item) => {
+            const current = Reflect.has(item, 'data') ? _omit(item, 'data') : item;
+
+            return JSON.stringify(
+                Object.entries(current).sort((a, b) => a[0] > b[0] ? -1 : 1),
+            );
+        });
+
+        return stringified.sort((a, b) => a > b ? -1 : 1).join(',');
+    }
+
+    private static isEventMatching<T>(filter: Partial<IEvent<T>>, event: IEvent<T>): boolean {
+        return _every(_keys(filter), (k: string) => filter[k] === event[k]);
     }
 }
