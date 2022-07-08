dc1b277466982538abc8e8dff1a949fd8f0bb477
Valentin Shlemin <valentin.shlemin@softgamings.com>
2022-07-08 17:05:05 +0300

    SCR #388299 - test: apitest jackposts

---
+++ api-tests/spec/jackpots.spec.ts
@@ -0,0 +1,34 @@
+import {
+    fetch,
+    getRequestUrl,
+} from './helpers/global';
+
+import {IJackpot} from 'wlc-engine/modules/games/system/interfaces/games.interfaces';
+import {IData} from 'wlc-engine/modules/core';
+
+describe('/api/v1/jackpots', () => {
+    const url = getRequestUrl('/api/v1/jackpots');
+    const interfaceName = 'IJackpot';
+
+    it('-> IJackpot', async (done: DoneFn): Promise<void> => {
+        fetch(url)
+            .then((res: Response) => res.json())
+            .then((response: IData<IJackpot[]>) => {
+                if (response.code !== 200) {
+                    throw response.errors;
+                }
+
+                if (response.data?.length === 0) {
+                    console.warn('\x1b[31m%s\x1b[0m', '\n  WARN: Jackpots are empty.');
+                } else {
+                    response.data?.forEach((element: IJackpot): void => {
+                        expect(interfaceName).toBeImplemented(element);
+                    });
+                }
+            })
+            .catch((err: unknown) => fail(err))
+            .finally(() => {
+                done();
+            });
+    });
+});
