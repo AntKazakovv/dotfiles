46fbabd8e895f0993ba0aa435c9c2a8bccf23d4c
Ivan Kuzmichev <ivan.kuzmichev@softgamings.com>
2022-07-27 15:44:56 +0300

    SCR #394184 - chore: made checks smarter

--- gulpfile/tasks/0004.checking-changes.task.js
+++ gulpfile/tasks/0004.checking-changes.task.js
@@ -1,12 +1,18 @@
 const {task} = require('gulp');
 
 module.exports = function checkingChanges() {
-    task('checking-changes', (cb) => {
-        const currentBranch = this.execNativeShellSync('git rev-parse --abbrev-ref HEAD').trim();
-        const diff = this.execNativeShellSync(`git diff origin/develop ${currentBranch} package-lock.json`);
+    task('checking-changes', async (cb) => {
+        const targetBranch = process.env.CI_MERGE_REQUEST_TARGET_BRANCH_NAME;
+        const diff =
+            this.execNativeShellSync(`git diff origin/${targetBranch} HEAD package-lock.json | wc -c | xargs`);
 
-        if (diff.length) {
-            throw new Error('package-lock does not match, please rebase the branch.');
+        if (+diff) {
+            console.error(
+                '-'.repeat(55) +
+                '\n\nPackage-lock does not match, please rebase the branch.\n\n' +
+                '-'.repeat(55),
+            );
+            process.exit(1);
         }
 
         cb();
