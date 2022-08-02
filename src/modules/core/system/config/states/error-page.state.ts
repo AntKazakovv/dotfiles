de13eca728e75100e369ae081ba752933eac1cf8
Aleksandr Korobkin <aleksandr.korobkin@softgamings.com>
2022-08-02 21:13:40 +0300

    SCR #383595 - refactor: error page

--- src/modules/core/system/config/states/error-page.state.ts
+++ src/modules/core/system/config/states/error-page.state.ts
@@ -1,30 +1,42 @@
 'use strict';
 
-import {Ng2StateDeclaration} from '@uirouter/angular';
-import {Subscription, interval} from 'rxjs';
-import {ConfigService, EventService, ModalService} from 'wlc-engine/modules/core';
+import {
+    Ng2StateDeclaration,
+    Transition,
+} from '@uirouter/angular';
+import {
+    Subscription,
+    interval,
+} from 'rxjs';
+
+import {
+    ConfigService,
+    EventService,
+    ModalService,
+} from 'wlc-engine/modules/core';
 
 let subs$: Subscription;
 let timeoutId: ReturnType<typeof setTimeout>;
+let timeout: number;
 
 export const errorPageState: Ng2StateDeclaration = {
     url: '/error',
-    onEnter: async (trans) => {
+    onEnter: async (trans: Transition) => {
         const configService: ConfigService = trans.injector().get(ConfigService);
         const eventService: EventService = trans.injector().get(EventService);
         const modalService: ModalService = trans.injector().get(ModalService);
         await configService.ready;
-        const timeout: number = configService.get<number>('$base.app.toHomeFromErrorTimeout');
-        const setTimeoutFn = (): void => {
+        timeout = configService.get<number>('$base.app.toHomeFromErrorTimeout');
+        const redirectToHome = (): void => {
             trans.router.stateService.go('app.home');
         };
         let openModal$: Subscription;
         let delay: number = timeout;
         let allTime: number = 0;
         let openModalTime: number = 0;
-        
+
         if (timeout) {
-            timeoutId = setTimeout(setTimeoutFn, delay);
+            timeoutId = setTimeout(redirectToHome, delay);
             subs$ = interval(1000).subscribe((): void => {
                 allTime++;
             });
@@ -46,14 +58,15 @@ export const errorPageState: Ng2StateDeclaration = {
                 if (openModal$) {
                     openModal$.unsubscribe();
                     delay = timeout - ((allTime - openModalTime) * 1000);
-                    timeoutId = setTimeout(setTimeoutFn, delay);
+                    timeoutId = setTimeout(redirectToHome, delay);
                 }
-            },
-            ));
+            }));
         }
     },
     onExit: (): void => {
-        clearTimeout(timeoutId);
-        subs$.unsubscribe();
+        if (timeout) {
+            clearTimeout(timeoutId);
+            subs$.unsubscribe();
+        }
     },
 };
