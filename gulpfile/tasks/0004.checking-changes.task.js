d1e15b9b75e1a51b9e1d9572b62478cbca81f089
Dmitry Zolotko <dmitry.zolotko@softgamings.com>
2022-08-03 12:39:18 +0300

    SCR #395248 - fix: pipline

--- gulpfile/tasks/0004.checking-changes.task.js
+++ gulpfile/tasks/0004.checking-changes.task.js
@@ -2,11 +2,12 @@ const {task} = require('gulp');
 
 module.exports = function checkingChanges() {
     task('checking-changes', async (cb) => {
-        const targetBranch = process.env.CI_MERGE_REQUEST_TARGET_BRANCH_NAME;
+        const source = process.env.CI_MERGE_REQUEST_SOURCE_BRANCH_NAME;
+        const target = process.env.CI_MERGE_REQUEST_TARGET_BRANCH_NAME;
         const diff =
-            this.execNativeShellSync(`git diff origin/${targetBranch} HEAD package-lock.json | wc -c | xargs`);
+            this.execNativeShellSync(`git diff origin/${target} origin/${source} package-lock.json | wc -c | xargs`);
 
-        if (!+diff) {
+        if (+diff) {
             console.error(
                 '-'.repeat(55) +
                 '\n\nPackage-lock does not match, please rebase the branch.\n\n' +
