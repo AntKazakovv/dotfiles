36b619eaa0f947951c61c4ed30b89b096b94ad88
Igor Shevchenko <igor.shevchenko@softgamings.com>
2022-10-31 07:11:32 +0000

    SCR #399334 - feat: Wp promo filter by auth

--- src/modules/static/components/wp-promo/wp-promo.component.ts
+++ src/modules/static/components/wp-promo/wp-promo.component.ts
@@ -17,10 +17,12 @@ import {
 import {WpPromoModel} from 'wlc-engine/modules/static/system/models/wp-promo.model';
 import {TextDataModel} from 'wlc-engine/modules/static/system/models/textdata.model';
 import {StaticService} from 'wlc-engine/modules/static/system/services/static/static.service';
+import {EventService} from 'wlc-engine/modules/core/system/services/event/event.service';
 
 import _map from 'lodash-es/map';
 import _sortBy from 'lodash-es/sortBy';
 import _assign from 'lodash-es/assign';
+import _filter from 'lodash-es/filter';
 
 import * as Params from './wp-promo.params';
 
@@ -34,6 +36,7 @@ export class WpPromoComponent extends AbstractComponent implements OnInit {
     @Input() public inlineParams: Params.IWpPromoCParams;
     public $params: Params.IWpPromoCParams;
     public items: WpPromoModel[] = [];
+    protected response: TextDataModel[];
 
     constructor(
         @Inject('injectParams') protected injectParams: Params.IWpPromoCParams,
@@ -42,6 +45,7 @@ export class WpPromoComponent extends AbstractComponent implements OnInit {
         protected modalService: ModalService,
         protected staticService: StaticService,
         protected logService: LogService,
+        protected eventService: EventService,
     ) {
         super(
             <IMixedParams<Params.IWpPromoCParams>>{
@@ -55,6 +59,7 @@ export class WpPromoComponent extends AbstractComponent implements OnInit {
     public ngOnInit(): void {
         super.ngOnInit(this.inlineParams);
         this.getItemsBySlug();
+        this.initEventHandlers();
     }
 
     /**
@@ -87,8 +92,8 @@ export class WpPromoComponent extends AbstractComponent implements OnInit {
      */
     protected async getItemsBySlug(): Promise<void> {
         try {
-            const response = await this.staticService.getPostsListByCategorySlug(this.$params.categorySlug);
-            this.items = this.sortItemsBySortWeight(this.prepareItems(response));
+            this.response = await this.staticService.getPostsListByCategorySlug(this.$params.categorySlug);
+            this.items = this.sortItemsBySortWeight(this.prepareItems(this.response));
         } catch (error) {
             this.logService.sendLog({
                 code: '5.0.4',
@@ -106,10 +111,40 @@ export class WpPromoComponent extends AbstractComponent implements OnInit {
      * @returns {WpPromoModel[]} items converted to WpPromoModel
      */
     protected prepareItems(items: TextDataModel[]): WpPromoModel[] {
-        return _map(items, (item: TextDataModel) => {
+        let filteredItems = this.filterItems(items);
+        return _map(filteredItems, (item: TextDataModel) => {
             return new WpPromoModel(
                 {component: 'WpPromoComponent', method: 'prepareItems'}, item, this.logService,
             );
         });
+    }
+
+    /**
+     * Filtered by visible_for ('all' | 'auth' | 'notAuth')
+     * @param {TextDataModel[]} items - takes TextDataModel array
+     * @returns {TextDataModel[]} items filtered by visible_for
+     */
+    protected filterItems(items: TextDataModel[]): TextDataModel[] {
+        let filteredItems: TextDataModel[] = _filter(items, (item: TextDataModel) => {
+            const visible = item.extFields.acf.visible_for;
+            return ((visible === 'all')
+                || (!visible)
+                || (visible === 'notAuth' && !this.configService.get<boolean>('$user.isAuthenticated'))
+                || (visible === 'auth' && this.configService.get<boolean>('$user.isAuthenticated')));
+        });
+        return filteredItems;
+    }
+
+    /**
+     * Init event handlers
+     */
+    protected initEventHandlers(): void {
+        this.eventService.subscribe([
+            {name: 'LOGIN'},
+            {name: 'LOGOUT'},
+        ], () => {
+            this.items = this.sortItemsBySortWeight(this.prepareItems(this.response));
+            this.cdr.markForCheck();
+        }, this.$destroy);
     }
 }
