102ee509c839782b1e713ff56789ac47bfd63615
Aleksandr Korobkin <aleksandr.korobkin@softgamings.com>
2023-03-22 14:01:46 +0000

    SCR #442897 - test: menu component

--- src/modules/menu/components/menu/menu.component.spec.ts
+++ src/modules/menu/components/menu/menu.component.spec.ts
@@ -2,7 +2,21 @@ import {
     ComponentFixture,
     TestBed,
 } from '@angular/core/testing';
-import {AppModule} from 'wlc-engine/modules/app/app.module';
+import {TranslateService} from '@ngx-translate/core';
+import {
+    StateService,
+    UIRouter,
+} from '@uirouter/core';
+
+import {Observable} from 'rxjs';
+
+import {
+    ActionService,
+    ConfigService,
+    DeviceType,
+    EventService,
+    ModalService,
+} from 'wlc-engine/modules/core';
 import {MenuComponent} from './menu.component';
 import {
     defaultParams,
@@ -12,6 +26,14 @@ import {
 describe('MenuComponent', () => {
     let component: MenuComponent;
     let fixture: ComponentFixture<MenuComponent>;
+
+    let configServiceSpy: jasmine.SpyObj<ConfigService>;
+    let actionServiceSpy: jasmine.SpyObj<ActionService>;
+    let modalServiceSpy: jasmine.SpyObj<ModalService>;
+    let eventServiceSpy: jasmine.SpyObj<EventService>;
+    let stateServiceSpy: jasmine.SpyObj<StateService>;
+    let translateServiceSpy: jasmine.SpyObj<TranslateService>;
+    let routerSpy: jasmine.SpyObj<UIRouter>;
 
     function createComponent(injectParams: IMenuCParams): void {
         TestBed.overrideProvider(
@@ -25,9 +47,56 @@ describe('MenuComponent', () => {
     }
 
     beforeEach(() => {
+        actionServiceSpy = jasmine.createSpyObj(
+            'ActionService',
+            ['getDeviceType', 'deviceType'],
+            {
+                device: {
+                    osName: 'ios',
+                    browserName: 'safari',
+                },
+            },
+        );
+        actionServiceSpy.deviceType.and.returnValue(new Observable<DeviceType>(() => {}));
+        configServiceSpy = jasmine.createSpyObj('ConfigService', ['get']);
+        eventServiceSpy = jasmine.createSpyObj('EventService', ['subscribe']);
+
         TestBed.configureTestingModule({
-            imports: [AppModule],
             declarations: [MenuComponent],
+            providers: [
+                {
+                    provide: ConfigService,
+                    useValue: configServiceSpy,
+                },
+                {
+                    provide: ActionService,
+                    useValue: actionServiceSpy,
+                },
+                {
+                    provide: ModalService,
+                    useValue: modalServiceSpy,
+                },
+                {
+                    provide: EventService,
+                    useValue: eventServiceSpy,
+                },
+                {
+                    provide: EventService,
+                    useValue: eventServiceSpy,
+                },
+                {
+                    provide: StateService,
+                    useValue: stateServiceSpy,
+                },
+                {
+                    provide: TranslateService,
+                    useValue: translateServiceSpy,
+                },
+                {
+                    provide: UIRouter,
+                    useValue: routerSpy,
+                },
+            ],
         }).compileComponents();
     });
 
