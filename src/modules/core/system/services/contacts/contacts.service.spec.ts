f85510c520d91cfd437f47d8384b3e62bfdeeb9e
Aleksandr Korobkin <aleksandr.korobkin@softgamings.com>
2023-04-11 05:47:24 +0000

    SCR #442883 - test: contacts service

--- src/modules/core/system/services/contacts/contacts.service.spec.ts
+++ src/modules/core/system/services/contacts/contacts.service.spec.ts
@@ -1,51 +1,52 @@
-import {
-    fakeAsync,
-    TestBed,
-    tick,
-} from '@angular/core/testing';
-import {
-    HttpClientTestingModule,
-    HttpTestingController,
-    TestRequest,
-} from '@angular/common/http/testing';
-import {AppModule} from 'wlc-engine/modules/app/app.module';
+import {TestBed} from '@angular/core/testing';
+
 import {ContactsService} from './contacts.service';
+import {DataService} from 'wlc-engine/modules/core/system/services/data/data.service';
 
 describe('ContactsService', () => {
     let contactsService: ContactsService;
-    let httpTestingController: HttpTestingController;
+    let dataServiceSpy: jasmine.SpyObj<DataService>;
 
     beforeEach(() => {
+        dataServiceSpy = jasmine.createSpyObj(
+            'DataService',
+            ['request'],
+        );
+
         TestBed.configureTestingModule({
-            imports: [AppModule, HttpClientTestingModule],
-            providers: [ContactsService],
+            providers: [
+                ContactsService,
+                {
+                    provide: DataService,
+                    useValue: dataServiceSpy,
+                },
+            ],
         });
 
         contactsService = TestBed.inject(ContactsService);
-        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
     it('-> should be created', () => {
         expect(contactsService).toBeTruthy();
     });
 
-    it('-> send: send e-mail', fakeAsync(() => {
+    it('-> send: send e-mail',() => {
         const testData = {
             senderName: 'Test Sender',
             senderEmail: 'test@sender.com',
             subject: 'Email subject',
             message: 'This is email message',
         };
+
         contactsService.send(testData);
-        tick();
 
-        const req: TestRequest = httpTestingController.expectOne(
-            '/api/v1/supportEmail?lang=en',
-            'request to send a letter',
-        );
-
-        expect(JSON.parse(req.request.body)).toEqual(testData);
-        expect(req.request.method).toEqual('POST');
-        expect(req.request.url).toEqual('/api/v1/supportEmail');
-    }));
+        expect(dataServiceSpy.request).toHaveBeenCalled();
+        expect(dataServiceSpy.request).toHaveBeenCalledTimes(1);
+        expect(dataServiceSpy.request).toHaveBeenCalledWith({
+            name: 'supportEmail',
+            system: 'supportEmail',
+            url: '/supportEmail',
+            type: 'POST',
+        }, testData);
+    });
 });
