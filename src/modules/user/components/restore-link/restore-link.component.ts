ed5a29a31116749070865924feac7e2c62a3a925
Stanislav Zhukov <stanislav.zhukov@softgamings.com>
2023-05-11 19:59:01 +0400

    SCR #474423 - refactor: restore-link onPush

--- src/modules/user/components/restore-link/restore-link.component.ts
+++ src/modules/user/components/restore-link/restore-link.component.ts
@@ -2,17 +2,17 @@ import {
     Component,
     OnInit,
     Inject,
+    ChangeDetectionStrategy,
 } from '@angular/core';
 import {ModalService} from 'wlc-engine/modules/core/system/services';
 import {AbstractComponent} from 'wlc-engine/modules/core/system/classes/abstract.component';
 import * as Params from './restore-link.params';
 
-// TODO:REFACTOR:change-detection-rule
-// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
     selector: '[wlc-restore-link]',
     templateUrl: './restore-link.component.html',
     styleUrls: ['./styles/restore-link.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RestoreLinkComponent extends AbstractComponent implements OnInit {
     public override $params: Params.IRestoreLinkCParams;
