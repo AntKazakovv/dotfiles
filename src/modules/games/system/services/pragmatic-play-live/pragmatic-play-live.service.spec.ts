ff91b4cc849b0b0da1ed07fc72802690513f275c
Aleksandr Korobkin <aleksandr.korobkin@softgamings.com>
2023-05-18 05:32:44 +0000

    SCR #479889 - test: pragmatic play live service

--- src/modules/games/system/services/pragmatic-play-live/pragmatic-play-live.service.spec.ts
+++ src/modules/games/system/services/pragmatic-play-live/pragmatic-play-live.service.spec.ts
@@ -1,15 +1,41 @@
 import {TestBed} from '@angular/core/testing';
-import {AppModule} from 'wlc-engine/modules/app/app.module';
 
+import {BehaviorSubject} from 'rxjs';
+
+import {
+    ConfigService,
+    EventService,
+} from 'wlc-engine/modules/core';
 import {PragmaticPlayLiveService} from './pragmatic-play-live.service';
 
 describe('PragmaticPlayLiveService', () => {
     let service: PragmaticPlayLiveService;
+    let configServiceSpy: jasmine.SpyObj<ConfigService>;
+    let eventServiceSpy: jasmine.SpyObj<EventService>;
 
     beforeEach(() => {
+        eventServiceSpy = jasmine.createSpyObj('EventService', ['subscribe']);
+        configServiceSpy = jasmine.createSpyObj(
+            'ConfigService',
+            ['load', 'get'],
+            {
+                'ready': Promise.resolve(),
+            },
+        );
+        configServiceSpy.get.and.returnValue(new BehaviorSubject(''));
+
         TestBed.configureTestingModule({
-            imports: [AppModule],
-            providers: [PragmaticPlayLiveService],
+            providers: [
+                {
+                    provide: ConfigService,
+                    useValue: configServiceSpy,
+                },
+                {
+                    provide: EventService,
+                    useValue: eventServiceSpy,
+                },
+                PragmaticPlayLiveService,
+            ],
         });
         service = TestBed.inject(PragmaticPlayLiveService);
     });
