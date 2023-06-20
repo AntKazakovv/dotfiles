ef46b8d39e28e305d9a05eb3b43a14167fb9f202
Konstantin Kondaurov <konstantin.kondaurov@softgamings.com>
2023-06-20 08:49:46 +0000

    SCR #485894 - fix: add fallback to translate loader

--- src/modules/core/system/config/translate.loader.ts
+++ src/modules/core/system/config/translate.loader.ts
@@ -1,12 +1,40 @@
 import {
     MissingTranslationHandler,
     MissingTranslationHandlerParams,
+    TranslateLoader,
 } from '@ngx-translate/core';
 import {HttpClient} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+import {Observable} from 'rxjs';
+import {
+    timeout,
+    catchError,
+} from 'rxjs/operators';
+
+export class TranslateFallbackHttpLoader implements TranslateLoader {
+
+    private static readonly WAIT_FOR_SERVER = 2000;
+
+    constructor(
+        private http: HttpClient,
+        private prefix: string,
+        private suffix: string,
+    ) {}
+
+    public getTranslation(lang: string): Observable<Object> {
+
+        return this.http.get(`${this.prefix}${lang}${this.suffix}`)
+            .pipe(
+                timeout(TranslateFallbackHttpLoader.WAIT_FOR_SERVER),
+                catchError(() => {
+                    return this.http.get(`${this.prefix}en${this.suffix}`);
+                }),
+            );
+    }
+}
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, './static/languages/', `.json?v=${WLC_VERSION}`);
+    return new TranslateFallbackHttpLoader(http, './static/languages/', `.json?v=${WLC_VERSION}`);
 }
 
 export class MissingTranslationService implements MissingTranslationHandler {
