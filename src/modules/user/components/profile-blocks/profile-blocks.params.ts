c59fa23738bbae8e717270b258cd164c2f58bf93
Danil Filippov <danil.filippov@softgamings.com>
2023-11-15 12:43:29 +0000

    SCR #500843 - feat: [wolf] profile blocks

--- src/modules/user/components/profile-blocks/profile-blocks.params.ts
+++ src/modules/user/components/profile-blocks/profile-blocks.params.ts
@@ -5,9 +5,10 @@ import {
 import {
     IInputCParams,
     ISelectCParams,
+    IButtonCParams,
 } from 'wlc-engine/modules/core';
 
-export type Theme = 'default' | CustomType;
+export type Theme = 'default' | 'wolf' | CustomType;
 export type Type = 'default' | CustomType;
 export type ThemeMod = 'default' | CustomType;
 
@@ -19,10 +20,42 @@ export interface IFieldComponentParams {
 }
 
 export interface IProfileBlocksCParams extends IComponentParams<Theme, Type, ThemeMod> {
+    profileBlockPassword?: {
+        title?: string,
+        subtitle?: string,
+        buttonParams?: IButtonCParams,
+    };
+    profileBlockBankInfo?: {
+        title?: string,
+        subtitle?: string,
+        buttonParams?: IButtonCParams,
+    };
 }
 
 export const defaultParams: IProfileBlocksCParams = {
     class: 'wlc-profile-blocks',
     componentName: 'wlc-profile-blocks',
     moduleName: 'user',
+    profileBlockPassword: {
+        title: gettext('Password'),
+        subtitle: gettext('You can change your password here.'),
+        buttonParams: {
+            wlcElement: 'button_change-password',
+            common: {
+                text: gettext('Change'),
+                iconPath: 'wlc/icons/arrow.svg',
+            },
+        },
+    },
+    profileBlockBankInfo: {
+        title: gettext('Banking information'),
+        subtitle: gettext('You can add your banking information here.'),
+        buttonParams: {
+            wlcElement: 'button_change-password',
+            common: {
+                text: gettext('Add info'),
+                iconPath: 'wlc/icons/arrow.svg',
+            },
+        },
+    },
 };
