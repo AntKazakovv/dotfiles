e885b1ceecbd10fcc7308f30882d80162a7cc012
Egor Prudnikov <egor.prudnikov@softgamings.com>
2024-03-19 10:16:33 +0000

    SCR #579305 - fix: injection store service in profile state

--- src/modules/core/system/config/states/profile/profile-store.state.ts
+++ src/modules/core/system/config/states/profile/profile-store.state.ts
@@ -3,6 +3,7 @@
 import {Ng2StateDeclaration} from '@uirouter/angular';
 import {Transition} from '@uirouter/core';
 
+import {InjectionService} from 'wlc-engine/modules/core/system/services/injection/injection.service';
 import {StoreService} from 'wlc-engine/modules/store/system/services/store/store.service';
 import {StoreCategory} from 'wlc-engine/modules/store/system/models/store-category.model';
 import {StateHelper} from 'wlc-engine/modules/core/system/helpers';
@@ -18,7 +19,8 @@ export const profileStoreState: Ng2StateDeclaration = {
 export const profileStoreMain: Ng2StateDeclaration = {
     url: '?category',
     onEnter: async (transition: Transition) => {
-        const storeService: StoreService = transition.injector().get(StoreService);
+        const injectionService: InjectionService = transition.injector().get(InjectionService);
+        const storeService: StoreService = await injectionService.getService('store.store-service');
         const category: StoreCategory = await storeService.getCategoryByState(transition);
 
         if (transition.params().category && !category) {
