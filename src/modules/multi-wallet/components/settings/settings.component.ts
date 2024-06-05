191b7bdd747f78d999b524ede63cf6e8c5348d31
Nadezhda Strokova <nadezhda.strokova@softgamings.com>
2024-06-05 09:06:00 +0000

    SCR #601961 - fix: style multiwallet

--- src/modules/multi-wallet/components/settings/settings.component.ts
+++ src/modules/multi-wallet/components/settings/settings.component.ts
@@ -12,6 +12,8 @@ import {
     AbstractComponent,
     ConfigService,
 } from 'wlc-engine/modules/core';
+
+import {CurrencyName} from 'wlc-engine/modules/currency/system/interfaces/currency.interface';
 import {WalletHelper} from 'wlc-engine/modules/multi-wallet';
 
 import * as Params from 'wlc-engine/modules/multi-wallet/components/settings/settings.params';
@@ -50,11 +52,18 @@ export class SettingsComponent extends AbstractComponent implements OnInit {
         this.$params.toggleViewFiat.onChange = (checked: boolean): void => {
             this.$params.walletSettings.conversionInFiat = checked;
         };
+
+        if (this.$params.walletSettings?.currency?.length) {
+            this.$params.currencies = this.$params.currencies
+                .sort((item: CurrencyName) => item.name === this.$params.walletSettings.currency ? -1 : 1);
+        }
         WalletHelper.walletSettings = this.$params.walletSettings;
     }
 
     public onCurrencyChange(currency: string): void {
         this.$params.walletSettings.currency = currency;
+        this.$params.currencies = this.$params.currencies
+            .sort((item: CurrencyName) => item.name === currency ? -1 : 1);
         this.cdr.markForCheck();
     }
 
