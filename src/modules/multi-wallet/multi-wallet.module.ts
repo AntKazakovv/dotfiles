84792b80dd731f95d502da26114d49e0cf5994d5
Nadezhda Strokova <nadezhda.strokova@softgamings.com>
2024-07-03 20:27:27 +0000

    SCR #617997 - refactor: remove CommonModule and TranslateModule from Multiwallet

--- src/modules/multi-wallet/multi-wallet.module.ts
+++ src/modules/multi-wallet/multi-wallet.module.ts
@@ -1,7 +1,5 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {TranslateModule} from '@ngx-translate/core';
 
 import {WalletsComponent} from './components/wallets/wallets.component';
 import {CoreModule} from 'wlc-engine/modules/core/core.module';
@@ -31,10 +29,8 @@ export const services = {
     ],
 
     imports: [
-        CommonModule,
         CoreModule,
         FormsModule,
-        TranslateModule,
     ],
     providers: [
         WalletsService,
