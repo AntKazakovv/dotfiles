32c04c5f1225d7c687816dd96064656a47d9baa8
Aleksei Smorodin <aleksei.smorodin@softgamings.com>
2024-10-01 10:02:32 +0300

    SCR #642505 - fix: fix adjusting transaction history page units

--- src/modules/history/system/config/history.config.ts
+++ src/modules/history/system/config/history.config.ts
@@ -73,11 +73,11 @@ export namespace transactionConfig {
             },
             {
                 value: 'deposit',
-                title: gettext('Deposit'),
+                title: gettext('Credit'),
             },
             {
                 value: 'withdraw',
-                title: gettext('Withdrawal'),
+                title: gettext('Debit'),
             },
             {
                 value: 'transfer',
