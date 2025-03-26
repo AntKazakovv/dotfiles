50f363fb4960a6b27352d6b818f3702797b0cbb6
Anton Kazakov <anton.kazakov@softgamings.com>
2025-03-26 17:26:28 +0100

    Add fish configs

---
+++ fish/.config/fish/conf.d/common.fish
@@ -0,0 +1,2 @@
+# set switch langs
+setxkbmap -layout "us,ru(unipunct)" -option "grp:alt_shift_toggle,lv3:ralt_switch,grp_led:scroll"
---
+++ fish/.config/fish/conf.d/env.fish
@@ -0,0 +1,5 @@
+fish_add_path (echo $HOME)/.local/share/fnm/
+fish_add_path /usr/local/go/bin/
+fish_add_path (echo $HOME)/go/bin/
+
+eval (fnm env)
