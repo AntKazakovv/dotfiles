f6314bb01fe4bd978ecd34c39782dd726371aebc
Anton Kazakov <anton.kazakov@softgamings.com>
2025-06-20 20:53:25 +0200

    Fish update

--- fish/.config/fish/conf.d/common.fish
+++ fish/.config/fish/conf.d/common.fish
@@ -1,2 +1,7 @@
 # set switch langs
 # setxkbmap -layout "us,ru(unipunct)" -option "grp:alt_shift_toggle,lv3:ralt_switch,grp_led:scroll"
+# 
+alias jt=just
+alias bat=batcat
+alias yz=yazi
+alias sc=scooter
