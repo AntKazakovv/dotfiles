288800aba2ef6b1bd04822a783f9d019d92141a6
Remi <toncon1@proton.me>
2026-03-17 21:20:11 +0100

    Add pear dir to PATH (fish)

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -3,3 +3,4 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/bin/
+fish_add_path /Users/remifo/Library/Application Support/pear/bin
