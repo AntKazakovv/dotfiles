1fbd2c517c2e5b323e51e682adda0910318ad31a
Remi <toncon1@proton.me>
2026-06-22 20:14:25 +0200

    Update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,11 @@ end
 if which lolcat
     lolcat ~/witch_acii.txt
 end
+
+sudo chown -R remifo /opt/homebrew /opt/homebrew/share/man/man6 /opt/homebrew/share/man/man7 /opt/homebrew/share/zsh /opt/homebrew/share/zsh/site-functions /opt/homebrew/var/homebrew/locks
+chmod u+w /opt/homebrew /opt/homebrew/share/man/man6 /opt/homebrew/share/man/man7 /opt/homebrew/share/zsh /opt/homebrew/share/zsh/site-functions /opt/homebrew/var/homebrew/locks
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/remifo/.lmstudio/bin
+# End of LM Studio CLI section
+
